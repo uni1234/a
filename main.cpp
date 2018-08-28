238588cd3ca4c39f79e4f6af48f1e1a32e9f2d3a
uni <mai3767@ezweb.ne.jp>
2018-08-29 05:54:16 +0900

    △

--- main.cpp
+++ main.cpp
@@ -1,13 +1,13 @@
 #include "camera.h"
 #include "image.h"
-#include "sphere.h"
+#include "triangle.h"
 #include "vec3.h"
 
 int main() {
   Image img(512, 512);
   Camera cam(Vec3(0, 0, -3), Vec3(0, 0, 1));
 
-  Sphere sphere(Vec3(0, 0, 0), 1.0);
+  Triangle triangle(Vec3(0, 0, 0), Vec3(1, 0, 0), Vec3(0, 1, 0));
 
   for (int i = 0; i < img.width; i++) {
     for (int j = 0; j < img.height; j++) {
@@ -15,7 +15,7 @@ int main() {
       double v = (2.0 * j - img.height) / img.height;
       Ray ray = cam.getRay(u, v);
 
-      if (sphere.intersect(ray)) {
+      if (triangle.intersect(ray)) {
         img.setPixel(i, j, Vec3(1, 1, 1));
       } else {
         img.setPixel(i, j, Vec3(0, 0, 0));
