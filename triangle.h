4a7f32483e425511416a23b6b95d45c83864e096
uni <mai3767@ezweb.ne.jp>
2018-08-29 05:53:28 +0900

    改良の余地あり(｀・ω・´)

--- triangle.h
+++ triangle.h
@@ -1,10 +1,41 @@
 #ifndef TRIANGLE_H
 #define TRIANGLE_H
+#include "ray.h"
+#include "vec3.h"
 
 class Triangle {
  public:
   Vec3 A;
   Vec3 B;
   Vec3 C;
-  bool intersect(const Ray& ray) {}
-}
+  Triangle(const Vec3& A, const Vec3& B, const Vec3& C) : A(A), B(B), C(C){};
+
+  bool intersect(const Ray& ray) const {
+    Vec3 a = A - B;
+    Vec3 b = C - B;
+    Vec3 n = normalize(cross(a, b));
+    Vec3 p = ray.origin - B;
+    Vec3 d = -1 * ray.direction;
+    if (dot(ray.direction, n) == 0) {
+      return false;
+    }
+    double k = det3(a, b, d);
+    double s = det3(p, b, d) / k;
+    double t = det3(a, p, d) / k;
+    double l = det3(a, b, p) / k;
+    if (s + t > 1 || s + t < 0) {
+      return false;
+    } else {
+      if (s > 1 || s < 0) {
+        return false;
+      } else {
+        if (t > 1 || t < 0) {
+          return false;
+        } else {
+          return true;
+        }
+      }
+    }
+  };
+};
+#endif
